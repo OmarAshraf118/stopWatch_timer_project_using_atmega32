546931d4ea4370f8e126806d777d53cf0830f8ae
OmarAshraf118 <168440891+OmarAshraf118@users.noreply.github.com>
2025-01-26 16:10:35 +0200

    Add files via upload

---
+++ Build/HAL/button.d
@@ -0,0 +1,7 @@
+button.o button.o: ../button.c ../gpio.h ../std_types.h ../button.h
+
+../gpio.h:
+
+../std_types.h:
+
+../button.h:
---
+++ Build/HAL/seven_segment_display.d
@@ -0,0 +1,9 @@
+seven_segment_display.o seven_segment_display.o:  \
+ ../seven_segment_display.c ../seven_segment_display.h ../std_types.h \
+  ../gpio.h
+
+../seven_segment_display.h:
+
+../std_types.h:
+
+../gpio.h:
