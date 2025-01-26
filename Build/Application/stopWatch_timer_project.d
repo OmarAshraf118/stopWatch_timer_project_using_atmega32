72255c05662f28ba4d8671d4a19daee663f2c220
OmarAshraf118 <168440891+OmarAshraf118@users.noreply.github.com>
2025-01-26 16:08:59 +0200

    Add files via upload

---
+++ Build/Application/stopWatch_timer_project.d
@@ -0,0 +1,17 @@
+stopWatch_timer_project.o stopWatch_timer_project.o:  \
+ ../stopWatch_timer_project.c ../timer.h ../std_types.h ../buzzer.h \
+  ../gpio.h ../seven_segment_display.h ../button.h ../led.h
+
+../timer.h:
+
+../std_types.h:
+
+../buzzer.h:
+
+../gpio.h:
+
+../seven_segment_display.h:
+
+../button.h:
+
+../led.h:
